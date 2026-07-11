7475827455e56fcee55981e78e6fb372e2ff79b5
NitinSharma <nitinsharma7991@gmail.com>
2026-07-11 23:59:12 +0530

    feat: add application bootstrap with config and logger

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"log"
+
+	"github.com/nitin-sharma-7991/aihub-backend/internal/config"
+	"github.com/nitin-sharma-7991/aihub-backend/internal/logger"
+)
+
 func main() {
 
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	logg, err := logger.New(cfg.App.LogLevel)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer logg.Sync()
+
+	logg.Info("Application Started")
+	logg.Info("Configuration Loaded")
 }
---
+++ internal/config/loader.go
@@ -0,0 +1,62 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+// Load reads configuration from environment variables
+// and returns the application configuration.
+func Load() (*Config, error) {
+
+	// Read values from .env file
+	viper.SetConfigFile(".env")
+	viper.SetConfigType("env")
+
+	// Environment variables override .env values
+	viper.AutomaticEnv()
+
+	// Load .env if available
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println("Warning: .env file not found. Using environment variables.")
+	}
+
+	cfg := &Config{
+		App: AppConfig{
+			Name:         viper.GetString("APP_NAME"),
+			Env:          viper.GetString("APP_ENV"),
+			Port:         viper.GetString("PORT"),
+			LogLevel:     viper.GetString("LOG_LEVEL"),
+			ReadTimeout:  viper.GetInt("READ_TIMEOUT"),
+			WriteTimeout: viper.GetInt("WRITE_TIMEOUT"),
+		},
+	}
+
+	// Basic Validation
+	if cfg.App.Name == "" {
+		return nil, fmt.Errorf("APP_NAME is required")
+	}
+
+	if cfg.App.Env == "" {
+		return nil, fmt.Errorf("APP_ENV is required")
+	}
+
+	if cfg.App.Port == "" {
+		return nil, fmt.Errorf("PORT is required")
+	}
+
+	if cfg.App.LogLevel == "" {
+		cfg.App.LogLevel = "info"
+	}
+
+	if cfg.App.ReadTimeout <= 0 {
+		cfg.App.ReadTimeout = 10
+	}
+
+	if cfg.App.WriteTimeout <= 0 {
+		cfg.App.WriteTimeout = 10
+	}
+
+	return cfg, nil
+}
